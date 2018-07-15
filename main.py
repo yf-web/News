d5d76559347369a4a766eed521696a67bedd8cb3
yf-web <yf_web@163.com>
2018-07-15 21:22:10 +0800

    添加配置信息

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+# 配置类
+class Config(object):
+    DEBUG=True
+    # 配置数据库
+    SQLALCHEMY_DATABASE_URI='mysql://root:74108520@127.0.0.1:3306/information'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+
+# 添加配置
+app.config.from_object(Config)
+
+db=SQLAlchemy(app,__name__)
 
 
 @app.route('/')
