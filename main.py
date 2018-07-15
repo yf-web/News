a2518bf4c2263b6d972d42f08cf5d875221db357
yf-web <yf_web@163.com>
2018-07-16 00:43:11 +0800

    配置session保存位置&测试是否可以保存到redis中

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
-app = Flask(__name__)
+# 可以用来设置session保存的位置
+from flask_session import Session
 
 
 # 配置类
@@ -16,6 +17,21 @@ class Config(object):
     REDIS_HOST='127.0.0.1'
     REDIS_PORT=6379
 
+    # 配置session
+    SECRET_KEY='BnfxjsrMd92t3mzBmT76L/j2+NkmFcKERbKW9uO9MIEC0DgWe7R+sstQ=='
+    # 指定session保存位置
+    SESSION_TYPE='redis'
+    # 指定目标redis
+    SESSION_REDIS=StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 开启session标签
+    SESSION_USE_SIGNER=True
+    # 关闭session永久有效
+    SESSION_PERMANENT=False
+    # 设置过期时间:2天
+    PERMANENT_SESSION_LIFETIME=86400*2
+
+
+app = Flask(__name__)
 
 # 添加配置
 app.config.from_object(Config)
@@ -29,11 +45,15 @@ redis_store=StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 # 开启CSRF
 CSRFProtect(app)
 
+# 设置session
+Session(app)
+
 
 @app.route('/')
 def index():
+    session['name']='hello'
     return 'index'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
