eb9e245c50e1ddfeefb40d11a2b0d1ac12b91efc
yf <yf_web@163.com>
2018-07-23 22:42:05 +0800

    完成收藏和取消收藏功能

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -2,6 +2,7 @@ from flask import abort, jsonify
 from flask import current_app
 from flask import g
 from flask import render_template
+from flask import request
 from flask import session
 
 from info import constants
@@ -44,11 +45,75 @@ def index(news_id):
     if not news:
         abort(404)
 
+    # 更新新闻点击次数
+    news.clicks+=1
+
+    # 判断新闻是否被登录的用户收藏
+    is_collected=False
+
+    # 先判断用户是否登录
+    if user:
+        # 登录后再判断是否收藏
+        if news in user.collection_news:
+            is_collected=True
 
     data={
         'user_info':user.to_dict() if user else None,
         'news_dict_list':news_dict_list,
-        'news':news.to_dict()
+        'news':news.to_dict(),
+        'is_collected':is_collected
 
     }
     return render_template('/news/detail.html',data=data)
+
+
+@news_blu.route('/news_collect',methods=['POST'])
+@user_login_data
+def collect():
+    """
+    实现收藏操作
+    :return:
+    """
+    # 先判断用户是否登录
+    user=g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR,errmsg='用户未登录')
+
+    # 接收参数
+    # news_id=json.loads(request.data).get('news_id')
+    news_id=request.json.get('news_id')
+    action=request.json.get('action')
+
+    # 校验参数
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR,errmsg='参数错误')
+
+    try:
+        news_id=int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+
+    if action not in ['cancel_collect','collect']:
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+
+    # 判断该新闻是否存在
+    try:
+        news=News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg='查询数据库异常')
+
+    if not news:
+        return jsonify(errno=RET.NODATA,errmsg='新闻不存在')
+
+    # 判断是取消还是收藏
+    if action=='collect':
+        # 判断该新闻是否被收藏
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+    else:
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+
+    return jsonify(errno=RET.OK,errmsg='收藏/取消收藏成功')
