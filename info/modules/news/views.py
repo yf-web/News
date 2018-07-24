69ba5e8c9844f654aeb7cd0ef9e37f03b5c8fadf
yf <yf_web@163.com>
2018-07-24 13:15:19 +0800

    完成收藏功能和评论功能

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -5,8 +5,8 @@ from flask import render_template
 from flask import request
 from flask import session
 
-from info import constants
-from info.models import News, User
+from info import constants, db
+from info.models import News, User, Comment
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
@@ -57,11 +57,24 @@ def index(news_id):
         if news in user.collection_news:
             is_collected=True
 
+    # 查询当前新闻下的评论信息
+    comments=[]  # 下面的comments可能不存在，预定义一下
+    try:
+        comments=Comment.query.filter(Comment.news_id==news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg='查询数据库错误')
+
+    comment_list=[]
+    for comment in comments:
+        comment_list.append(comment.to_dict())
+
     data={
         'user_info':user.to_dict() if user else None,
         'news_dict_list':news_dict_list,
         'news':news.to_dict(),
-        'is_collected':is_collected
+        'is_collected':is_collected,
+        'comment_list':comment_list
 
     }
     return render_template('/news/detail.html',data=data)
@@ -117,3 +130,69 @@ def collect():
             user.collection_news.remove(news)
 
     return jsonify(errno=RET.OK,errmsg='收藏/取消收藏成功')
+
+
+@news_blu.route('/news_comment',methods=['POST'])
+@user_login_data
+def comment():
+    """
+    评论功能分析：
+    １．评论新闻
+    接收评论新闻的id、评论内容
+    ２．评论其他用户的评论
+    接收评论新闻的id、评论内容＋其他用户评论的id
+    所以：同时实现评论新闻和评论其他用户的评论
+    :return:
+    """
+    # 先判断用户是否登录
+    user=g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR,errmsg='用户未登录')
+
+    # 接收参数
+    news_id=request.json.get('news_id')
+    comment_content=request.json.get('comment')
+    parent_id=request.json.get('parent_id')
+
+    # 校验参数--parent_id可有可无
+    if not all([news_id,comment_content]):
+        return jsonify(errno=RET.PARAMERR,errmsg='参数错误1')
+
+    try:
+        news_id=int(news_id)
+        if parent_id:
+            parent_id=int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误2')
+
+    # 判断该新闻是否存在
+    try:
+        news=News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询数据库异常')
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg='新闻不存在')
+
+    # 保存评论内容
+    comment=Comment()
+    comment.news_id=news_id
+    comment.content=comment_content
+    comment.parent_id=parent_id
+    comment.user_id=user.id
+
+    #　这里为什么手动提交？因为返回数据中需要使用到提交的数据，如果依赖自动提交，评论数据是在请求完成之后提交的
+    # 则无法获得提交的评论数据
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='保存数据异常')
+
+    data={
+        'comment':comment.to_dict()  # 该字典中的内容还包含user相关信息
+    }
+    return jsonify(errno=RET.OK,errmsg='评论成功',data=data)
--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -9,14 +9,14 @@ $(function(){
     // 打开登录框
     $('.comment_form_logout').click(function () {
         $('.login_form_con').show();
-    })
+    });
 
     // 收藏
     $(".collection").click(function () {
         var params={
             'news_id':$(this).attr('data-newid'),
             'action':'collect'
-        }
+        };
         $.ajax({
             url:'/news/news_collect',
             type:'POSt',
@@ -42,7 +42,7 @@ $(function(){
             }
         })
        
-    })
+    });
 
     // 取消收藏
     $(".collected").click(function () {
@@ -75,11 +75,73 @@ $(function(){
             }
         })
      
-    })
+    });
 
         // 评论提交
     $(".comment_form").submit(function (e) {
         e.preventDefault();
+
+        var news_id = $(this).attr('data-newsid');
+        var news_comment = $(".comment_input").val();
+
+        if (!news_comment) {
+            alert('请输入评论内容');
+            return
+        }
+        var params = {
+            "news_id": news_id,
+            "comment": news_comment
+        };
+        $.ajax({
+            url: "/news/news_comment",
+            type: "post",
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            data: JSON.stringify(params),
+            success: function (resp) {
+                if (resp.errno == '0') {
+                    var comment = resp.data.comment;
+                    // 拼接内容
+                    var comment_html = '';
+                    comment_html += '<div class="comment_list">';
+                    comment_html += '<div class="person_pic fl">';
+                    if(comment.user.avatar_url){
+                        comment_html += '<img src="' + comment.user.avatar_url + '" alt="用户图标">'
+                    }
+                    else{
+                        comment_html += '<img src="../../static/news/images/person01.png" alt="用户图标">'
+                    }
+                    comment_html += '</div>';
+                    comment_html += '<div class="user_name fl">' + comment.user.nick_name + '</div>';
+                    comment_html += '<div class="comment_text fl">';
+                    comment_html += comment.content;
+                    comment_html += '</div>';
+                    comment_html += '<div class="comment_time fl">' + comment.create_time + '</div>';
+
+                    comment_html += '<a href="javascript:;" class="comment_up fr" comment_id="' + comment.id + '" news_id="' + comment.news_id + '">赞</a>';
+                    comment_html += '<a href="javascript:;" class="comment_reply fr">回复</a>';
+                    comment_html += '<form class="reply_form fl" data-commentid="' + comment.id + '" data-newsid="' + news_id + '">';
+                    comment_html += '<textarea class="reply_input"></textarea>';
+                    comment_html += '<input type="button" value="回复" class="reply_sub fr">';
+                    comment_html += '<input type="reset" name="" value="取消" class="reply_cancel fr">';
+                    comment_html += '</form>';
+
+                    comment_html += '</div>';
+                    $(".comment_list_con").prepend(comment_html);
+                    $('.comment_sub').blur();
+
+                    // 清空输入框内容
+                    $(".comment_input").val("");
+
+                    // 更新评论条数
+                    commentCount();
+                }else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
 
     })
 
@@ -111,7 +173,79 @@ $(function(){
 
         if(sHandler.indexOf('reply_sub')>=0)
         {
-            alert('回复评论')
+           //回复子评论
+            var $this = $(this);
+            var news_id = $this.parent().attr('data-newsid');
+            var parent_id = $this.parent().attr('data-commentid');
+            var news_comment = $this.prev().val();
+
+            if (!news_comment) {
+                alert('请输入评论内容');
+                return
+            }
+            var params = {
+                "news_id": news_id,
+                "comment": news_comment,
+                "parent_id": parent_id
+            };
+            $.ajax({
+                url: "/news/news_comment",
+                type: "post",
+                contentType: "application/json",
+                headers: {
+                    "X-CSRFToken": getCookie("csrf_token")
+                },
+                data: JSON.stringify(params),
+                success: function (resp) {
+                    if (resp.errno == "0") {
+                        var comment = resp.data.comment;
+                        // 拼接内容
+                        var comment_html = "";
+                        comment_html += '<div class="comment_list">';
+                        comment_html += '<div class="person_pic fl">';
+
+                        if(comment.user.avatar_url){
+                            comment_html += '<img src="' + comment.user.avatar_url + '" alt="用户图标">'
+                        }
+                        else{
+                            comment_html += '<img src="../../static/news/images/person01.png" alt="用户图标">'
+                        }
+                        comment_html += '</div>';
+                        comment_html += '<div class="user_name fl">' + comment.user.nick_name + '</div>';
+                        comment_html += '<div class="comment_text fl">';
+                        comment_html += comment.content;
+                        comment_html += '</div>';
+                        comment_html += '<div class="reply_text_con fl">';
+                        comment_html += '<div class="user_name2">' + comment.parent.user.nick_name + '</div>';
+                        comment_html += '<div class="reply_text">';
+                        comment_html += comment.parent.content;
+                        comment_html += '</div>';
+                        comment_html += '</div>';
+                        comment_html += '<div class="comment_time fl">' + comment.create_time + '</div>';
+
+                        comment_html += '<a href="javascript:;" class="comment_up fr" data-commentid="' + comment.id + '" data-newsid="' + comment.news_id + '">赞</a>';
+                        comment_html += '<a href="javascript:;" class="comment_reply fr">回复</a>';
+                        comment_html += '<form class="reply_form fl" data-commentid="' + comment.id + '" data-newsid="' + news_id + '">';
+                        comment_html += '<textarea class="reply_input"></textarea>';
+                        comment_html += '<input type="button" value="回复" class="reply_sub fr">';
+                        comment_html += '<input type="reset" name="" value="取消" class="reply_cancel fr">';
+                        comment_html += '</form>';
+
+                        comment_html += '</div>';
+                        $(".comment_list_con").prepend(comment_html);
+                        // 请空输入框
+                        $this.prev().val('');
+                        // 关闭
+                        $this.parent().hide();
+                        // 更新评论条数
+                        commentCount();
+                    }else {
+                        alert(resp.errmsg)
+                    }
+                }
+            })
+
+
         }
     })
 
@@ -125,3 +259,8 @@ $(function(){
 
     })
 })
+
+function commentCount() {
+    var count=$('.comment_list').length
+    $('.comment_count').html(count+'条评论')
+}
